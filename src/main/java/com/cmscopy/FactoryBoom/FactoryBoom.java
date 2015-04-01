e5ec1e6053b5a38afa78732f36c3e5783dc521ef
cmscopy <mail@cmscopy.de>
2015-04-01 10:36:18 +0200

    first class

---
+++ src/main/java/com/cmscopy/FactoryBoom/FactoryBoom.java
@@ -0,0 +1,11 @@
+package com.cmscopy.FactoryBoom;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Created by clemens on 01.04.15.
+ */
+@Mod(modid= "FactoryBoom", name="Factory Boom", version="1.7.10-0.1A")
+public class FactoryBoom
+{
+}
